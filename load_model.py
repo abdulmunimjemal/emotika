59d0c0777947b3164810bcf48c7222652a5aa12e
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-13 01:14:04 +0300

    Added Model loading scripts

--- load_model.py
+++ load_model.py
@@ -10,7 +10,7 @@ def get_model() -> list:
     """
     # Load the model
     model = load_model('models/NN_model.h5')
-    tokenizer = pickle.load(open('models/NN_tokenizer.pkl', 'rb'))
+    tokenizer = pickle.load(open('models/NN_tokenizer.pickle', 'rb'))
     return model, tokenizer
 
 
