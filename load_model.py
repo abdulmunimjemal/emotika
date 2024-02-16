88c3b42a5550edd0ebe4c9c0c1b0248ff51083fa
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-02-17 02:14:49 +0300

    Added Reusability to model loading functions

--- load_model.py
+++ load_model.py
@@ -3,29 +3,38 @@ from tensorflow.keras.models import load_model
 import pickle
 
 
-def get_model() -> list:
+def get_model(model="NN_model.h5", tokenizer="NN_tokenizer.pickle") -> list:
     """
     Load the model and tokenizer
     :return: model and tokenizer
     """
     # Load the model
-    model = load_model('models/NN_model.h5')
-    tokenizer = pickle.load(open('models/NN_tokenizer.pickle', 'rb'))
+    model = load_model(f'models/{model}')
+    tokenizer = pickle.load(open(f'models/{tokenizer}', 'rb'))
     return model, tokenizer
 
 
-def emotion_decoder(emotion: int) -> str:
+def emotion_decoder(emotion: int, include_emojis: bool = True) -> str:
     """
     Decode the emotion from the integer value
     :param emotion: integer value of the emotion
     :return: string value of the emotion
     """
     emotions_map = {
-        0: 'joy',
-        1: 'sadness',
-        2: 'anger',
-        3: 'fear',
-        4: 'love',
-        5: 'surprise',
+        0: 'Joy',
+        1: 'Sadness',
+        2: 'Anger',
+        3: 'Fear',
+        4: 'Love',
+        5: 'Surprise',
     }
-    return emotions_map[emotion]
+
+    emotions_map_emojis = {
+        0: 'Joyful 😊',
+        1: 'Sad 😢',
+        2: 'Angry 😠',
+        3: 'Fear 😨',
+        4: 'Love it 😍',
+        5: 'Wow! Surprise 😲',
+    }
+    return emotions_map[emotion] if not include_emojis else emotions_map_emojis[emotion]
